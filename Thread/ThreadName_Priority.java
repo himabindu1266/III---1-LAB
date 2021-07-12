ae1c904dcced758d8b004f01613830e567299464
HIMA BINDU K V <69143645+himabindu1266@users.noreply.github.com>
2021-07-12 10:01:35 +0530

    Add files via upload

---
+++ Thread/ThreadName_Priority.java
@@ -0,0 +1,8 @@
+class ThreadName_Priority
+{
+public static void main (String args[])
+{
+System.out.println("Thread Name = " + Thread.currentThread().getName());
+System.out.println(" Priority = " + Thread.currentThread().getPriority());
+}
+} 
